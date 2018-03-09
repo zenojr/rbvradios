ec87f9f1d71706cc9e9a7042d3ce52c6275a09d4
Zeno França Junior <zfrancajunior@gmail.com>
2018-03-09 09:22:37 -0300

    db audio Add radios

--- rbvradios.com/src/app/dbaudio/dbaudio.component.ts
+++ rbvradios.com/src/app/dbaudio/dbaudio.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DbaudioComponent implements OnInit {
 
+  mailToTangara: string = "mailto:opec@rbvradios.com.br?Subject=Gravar%Audio";
+
   constructor() { }
 
   ngOnInit() {
